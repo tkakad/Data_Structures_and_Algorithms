04455c4362ff603c7904dee6fb43a11f80447092
Tejas Kakad <tejaskakad@Tejass-MacBook-Pro.local>
2022-11-06 20:50:28 -0600

    Completed BST with delete function and a function converting preorder traversal to BST

--- BST/BST.cpp
+++ BST/BST.cpp
@@ -1,41 +1,95 @@
 #include "BST.h"
 
 int main(){
-    int num;
-    vector<int> numbers = {9, 15, 5, 20, 16, 8, 12, 3, 6};
-    TreeNode *tree = create_BST(numbers);
-    // TreeNode *tree = create_tree();
+    // pre-order traversal to BST
 
+    TreeNode *temp_tree = BST_from_preorder({30,20,10,15,25,40,50,45});
     cout << "\nInorder traversal: ";
-    inorder_traversal(tree);
+    inorder_traversal(temp_tree);
     cout << endl;
-    level_order_traversal(tree);
+    level_order_traversal(temp_tree);
     cout << endl;
 
-    // Recursive insertion function needs to be fixed
+    // int temp;
+    // TreeNode *tree = NULL;
+    // tree = recursive_insert_BST(tree, 10);
+    // recursive_insert_BST(tree, 5);
+    // recursive_insert_BST(tree, 20);
+    // recursive_insert_BST(tree, 8);
+    // recursive_insert_BST(tree, 30);
 
-    // cout << "Enter the number to be inserted into the BST (recursively): ";
-    // cin >> num; 
-    // tree = recursive_insert_BST(tree, num);
+    // // int num;
+    // // vector<int> numbers = {9, 15, 5, 20, 16, 8, 12, 3, 6};
+    // // TreeNode *tree = create_BST(numbers);
+    
+    // // TreeNode *tree = create_tree();
+
+    // cout << "\nInorder traversal: ";
+    // inorder_traversal(tree);
+    // cout << endl;
+    // level_order_traversal(tree);
+    // cout << endl;
+
+    // // Recursive insertion function needs to be fixed
+
+    // // cout << "Enter the number to be inserted into the BST (recursively): ";
+    // // cin >> num; 
+    // // tree = recursive_insert_BST(tree, num);
+    // // cout << "Inorder traversal: ";
+    // // inorder_traversal(tree);
+    // // cout << endl;
+    // // level_order_traversal(tree);
+    // // cout << endl;
+
+    // tree = iterative_insert_BST(tree);
     // cout << "Inorder traversal: ";
     // inorder_traversal(tree);
     // cout << endl;
     // level_order_traversal(tree);
     // cout << endl;
 
-    tree = iterative_insert_BST(tree);
-    cout << "Inorder traversal: ";
-    inorder_traversal(tree);
-    cout << endl;
-    level_order_traversal(tree);
-    cout << endl;
+    // tree = iterative_insert_BST(tree);
+    // cout << "Inorder traversal: ";
+    // inorder_traversal(tree);
+    // cout << endl;
+    // level_order_traversal(tree);
+    // cout << endl;
 
-    tree = iterative_insert_BST(tree);
-    cout << "Inorder traversal: ";
-    inorder_traversal(tree);
-    cout << endl;
-    level_order_traversal(tree);
-    cout << endl;
+    // cout << "Enter the number to be deleted: ";
+    // cin >> temp;
+    // delete_node(tree, temp);
+    // cout << "Inorder traversal: ";
+    // inorder_traversal(tree);
+    // cout << endl;
+    // level_order_traversal(tree);
+    // cout << endl;
+
+    // cout << "Enter the number to be deleted: ";
+    // cin >> temp;
+    // delete_node(tree, temp);
+    // cout << "Inorder traversal: ";
+    // inorder_traversal(tree);
+    // cout << endl;
+    // level_order_traversal(tree);
+    // cout << endl;
+
+    // cout << "Enter the number to be deleted: ";
+    // cin >> temp;
+    // delete_node(tree, temp);
+    // cout << "Inorder traversal: ";
+    // inorder_traversal(tree);
+    // cout << endl;
+    // level_order_traversal(tree);
+    // cout << endl;
+
+    // cout << "Enter the number to be deleted: ";
+    // cin >> temp;
+    // delete_node(tree, temp);
+    // cout << "Inorder traversal: ";
+    // inorder_traversal(tree);
+    // cout << endl;
+    // level_order_traversal(tree);
+    // cout << endl;
 
     return 0;
 }
--- BST/BST.h
+++ BST/BST.h
@@ -31,6 +31,63 @@ struct Node{
     }
 };
 
+struct Stack{    
+    Node *top;
+    
+    Stack(){
+        top = NULL;
+    }
+
+    bool isFull(){
+        Node *t = new Node;
+
+        if(t){
+            delete t;
+            return false;
+        }
+        else{
+            delete t;
+            return true;
+        }
+    }
+
+    bool isEmpty(){
+        return !top;
+    }
+
+    void push(TreeNode *x){
+        if(!isFull()){
+            Node *t = new Node(x);
+            t->next = top;
+            top = t;
+        }
+    }
+
+    TreeNode *pop(){
+        if(!isEmpty()){
+            TreeNode *t = top->data;
+            Node *p = top;
+            top = top->next;
+            delete p;
+            return t;
+        }
+        return NULL;
+    }
+
+    void display(){
+        Node *temp = top;
+
+        cout << "Stack: ";
+
+        while(temp){
+            cout << temp->data->data << " ";
+            temp = temp->next;
+        }
+
+        cout << endl;
+    }
+};
+
 struct Queue{
     Node *front, *rear;
 
@@ -257,6 +314,22 @@ TreeNode *iterative_insert_BST(TreeNode *root){
     return root;
 }
 
+TreeNode *recursive_insert_BST(TreeNode *root, int x){
+    if(!root){
+        TreeNode* t = new TreeNode(x);
+        return t;
+    }
+
+    if(x == root->data)
+        return root;
+    else if(x < root->data)
+        root->left = recursive_insert_BST(root->left, x);
+    else
+        root->right = recursive_insert_BST(root->right, x);
+    
+    return root;
+}
+
 TreeNode *recursive_search(TreeNode* tree){
     int x;
 
@@ -292,4 +365,95 @@ TreeNode *iterative_search(TreeNode* tree){
     }
 
     return NULL;
+}
+
+TreeNode *inorder_predecessor(TreeNode *t){
+    if(!t)
+        return NULL;
+
+    while(t->right)
+        t = t->right;
+
+    return t;
+}
+
+TreeNode *inorder_successor(TreeNode *t){
+    if(!t)
+        return NULL;
+
+    while(t->left)
+        t = t->left;
+
+    return t;   
+}
+
+int tree_height(TreeNode *root){
+    if(!root)
+        return 0;
+
+    int x = tree_height(root->left), y = tree_height(root->right);    
+
+    return (x > y) ? (x + 1) : (y + 1);
+}
+
+TreeNode *delete_node(TreeNode* tree, int x){
+    if(!tree)
+        return NULL;
+    
+    if(!tree->left && !tree->right){
+        delete tree;
+        return NULL;
+    }
+    
+    if(x == tree->data){
+        TreeNode *t = NULL;
+
+        if(tree_height(tree->left) > tree_height(tree->right)){
+            t = inorder_predecessor(tree->left);
+            tree->data = t->data;
+            tree->left = delete_node(tree->left, t->data);
+        }
+        else{
+            t = inorder_successor(tree->right);
+            tree->data = t->data;
+            tree->right = delete_node(tree->right, t->data);
+        }
+    }
+    else if(x < tree->data)
+        tree->left = delete_node(tree->left, x);
+    else 
+        tree->right = delete_node(tree->right, x);
+    
+    return tree;
+}
+
+TreeNode *BST_from_preorder(vector<int> preorder_list){
+    vector<int> inorder_list = preorder_list;
+    sort(inorder_list.begin(), inorder_list.end());
+
+    TreeNode *root = new TreeNode(preorder_list[0]);
+    TreeNode *p = root;
+
+    Stack s;
+    s.push(root);
+
+    for(int i = 1; i < preorder_list.size(); i++){
+        TreeNode *t = new TreeNode(preorder_list[i]);
+
+        if(preorder_list[i] < p->data){
+            s.push(t);
+            p->left = t;
+        }
+        else{
+            while(s.top && (preorder_list[i] > ((s.top)->data)->data)){
+                p = s.pop();
+            }
+
+            p->right = t;
+        }
+
+        p = t;
+    }
+
+    return root;
 }
